85f4ab6ea423eabe780f0607607cf72e1153debd
erikgonzalez <erik.gonzalez.ac@gmail.com>
2019-10-22 19:57:59 -0700

    Modelos y servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { routing, appRoutingProviders } from './app.routing';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
 
